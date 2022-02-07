f68e196a6c62af62256d545dcc5c86793b92ad35
dereklowlind <38629827+dereklowlind@users.noreply.github.com>
2022-02-06 16:30:28 -0800

    the big wham

---
+++ run_fastai.py
@@ -0,0 +1,8 @@
+from fastbook import *
+# from fastai.vision.widgets import *
+
+learn_inf = load_learner('export.pkl')
+
+img1 = 'cat-scratching/scratching/out-59.jpg'
+
+# print(learn_inf.predict(img1))
